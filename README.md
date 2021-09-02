943f8f80fcd5e8f6a60e3a6dd425df0d1ae56144
nakata5321 <makar5321@gmail.com>
2021-09-02 18:20:18 +0300

    refactoring code

--- README.md
+++ README.md
@@ -7,12 +7,12 @@ Example of how it works is available on [YouTube.][db1]
  - ROS Melodic + Gazebo (installation manual [here][db2])  
  - extra packages:
 ```sh
-sudo apt-get install ros-melodic-gazebo-ros-control ros-melodic-effort-controllers ros-melodic-joint-state-controller
+sudo apt-get install ros-melodic-gazebo-ros-control ros-melodic-effort-controllers ros-melodic-joint-state-controller python3-rospkg rosbash
 ```
-- IPFS 0.4.22 (download from [here][db3] and install)
-- ipfshttpclient:
+- IPFS up to 0.6.0 (download from [here][db3] and install)
+- python packages:
 ```sh
-pip install ipfshttpclient
+pip3 install -r requirements.txt
 ```
  - Robonomics node (binary file) (download latest [release][db4] here)
  - IPFS browser extension (not necessary)
@@ -27,14 +27,13 @@ git clone https://github.com/nakata5321/Baxter_simulation_controller.git
 cd Baxter_simulation_controller
 git checkout old_version
 cd ../..
-catkin build
+catkin build -DPYTHON_EXECUTABLE=/usr/bin/python3
 ```
 Dont forget to add source command:
 ```sh
 echo "source /home/$USER/robot_ws/devel/setup.bash" >> ~/.bashrc
 source ~/.bashrc
-```  
-__Important!__ At the end save *Robonomics node (binary file)* in **robot_ws** directory.
+```
 
 ## 2. Start simulation
 Let's start gazebo world and put our baxter in it:
@@ -48,22 +47,20 @@ Open one more window in terminal:
 rosrun gazebo_ros spawn_model -file `rospack find baxter_description`/urdf/baxter.urdf -urdf -z 1 -model baxter
 ```
 You can put some models in front of our baxter. It will be more intresting.
+
 ![baxter][im2]
 
 ## 3.Manage accounts in DAPP
 
 Since we are testing, let us create a local robonomics network with robonomics binary file. Go to folder with robonomics file and run:
 ```sh
-./robonomics --dev --rpc-cors all
+./robonomics --dev --tmp --rpc-cors all
 ```
 ![robonomics][im3]
 
-Don't forget to remove `db` folder after every launch:
-```sh
-rm -rf /home/$USER/.local/share/robonomics/chains/dev/db
-```
 
 Go to [https://parachain.robonomics.network][db5] and switch to local node
+
 ![local node][im4]
 
 Go to Accounts and create __Baxter__ and __Employer__ accounts (__Robot__ is not necessary)
@@ -74,7 +71,7 @@ Transfer some money (units) to these accounts:
 ![create account][im5]
 ![accounts][im6]
 
-Add Baxter's secret key and adress to `configuration.txt` in `robot_ws/src/robot_controller/src/`
+Add Baxter's secret key and adress to `config.yaml` in `robot_ws/src/robot_controller/config/`
 
 ## 4.Start simulation
 
@@ -116,7 +113,7 @@ Click  __View on Gateway__ and that's all!
 
 [db1]: <https://youtu.be/AeufQmaNRWk>
 [db2]: <http://wiki.ros.org/melodic/Installation>
-[db3]: <https://dist.ipfs.io/go-ipfs/v0.4.22/go-ipfs_v0.4.22_linux-386.tar.gz>
+[db3]: <https://dist.ipfs.io/go-ipfs/v0.6.0/go-ipfs_v0.6.0_linux-386.tar.gz>
 [db4]: <https://github.com/airalab/robonomics/releases>
 [im1]: <https://github.com/nakata5321/Baxter_simulation_controller/blob/master/docs/images/empty_world.jpg>
 [im2]: <https://github.com/nakata5321/Baxter_simulation_controller/blob/master/docs/images/baxter_simulation.jpg>
--- robot_controller/src/robot_control.py
+++ robot_controller/src/robot_control.py
@@ -1,22 +1,303 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+
+# from cv_bridge.boost.cv_bridge_boost import getCvType
 
 import ipfshttpclient
-import subprocess
+import typing as tp
+import threading
+# import subprocess
 import rospy
+import yaml
 import time
 import cv2
 import os
+
+from substrateinterface import SubstrateInterface, Keypair
 from sensor_msgs.msg import LaserScan
-from geometry_msgs.msg import Twist
+# from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 from threading import Thread
 
-# write about activation
+
+def read_config(path: str) -> tp.Dict[str, str]:
+    try:
+        with open(path) as f:
+            content = f.read()
+            config = yaml.load(content)
+            rospy.logdebug(f"Configuration dict: {content}")
+            return config
+    except Exception as e:
+        while True:
+            rospy.logerr("Configuration file is broken or not readable!")
+            rospy.logerr(e)
+            rospy.sleep(5)
+
+
+def push_to_ipfs(result_picture: list, dirname: str) -> list:
+    rospy.loginfo("Push to IPFS")
+    client = ipfshttpclient.connect()
+    hash_result = []
+    for x in result_picture:
+        res = client.add(x)
+        hash_result.append(res.values()[0].encode('utf8'))
+        rospy.loginfo("Pushed, the IPFS hash is " + res.values()[0].encode('utf8'))
+    res = client.add(dirname + '/' + "result.txt")
+    hash_result.append(res.values()[0].encode('utf8'))
+    rospy.loginfo("Pushed, the IPFS hash is " + res.values()[0].encode('utf8'))
+    return hash_result
+
+
+def substrate_connection(url: str) -> tp.Any:
+    """
+    establish connection to a specified substrate node
+    """
+    try:
+        rospy.loginfo("Establishing connection to substrate node")
+        substrate = SubstrateInterface(
+            url=url,
+            ss58_format=32,
+            type_registry_preset="substrate-node-template",
+            type_registry={
+                "types": {
+                    "Record": "Vec<u8>",
+                    "Parameter": "Bool",
+                    "LaunchParameter": "Bool",
+                    "<T as frame_system::Config>::AccountId": "AccountId",
+                    "RingBufferItem": {
+                        "type": "struct",
+                        "type_mapping": [
+                            ["timestamp", "Compact<u64>"],
+                            ["payload", "Vec<u8>"],
+                        ],
+                    },
+                    "RingBufferIndex": {
+                        "type": "struct",
+                        "type_mapping": [
+                            ["start", "Compact<u64>"],
+                            ["end", "Compact<u64>"],
+                        ],
+                    }
+                }
+            },
+        )
+        rospy.loginfo("Successfully established connection to substrate node")
+        return substrate
+    except Exception as e:
+        rospy.logerr(f"Failed to connect to substrate: {e}")
+        return None
+
+
+def write_datalog(substrate, seed: str, data: str) -> str or None:
+    """
+    Write any string to datalog
+    Parameters
+    ----------
+    substrate : substrate connection instance
+    seed : mnemonic seed of account which writes datalog
+    data : data to be stored as datalog
+    Returns
+    -------
+    Hash of the datalog transaction
+    """
+
+    # create keypair
+    try:
+        keypair = Keypair.create_from_mnemonic(seed, ss58_format=32)
+    except Exception as e:
+        rospy.logerr(f"Failed to create keypair for recording datalog: \n{e}")
+        return None
+
+    try:
+        rospy.loginfo("Creating substrate call for recording datalog")
+        call = substrate.compose_call(
+            call_module="Datalog",
+            call_function="record",
+            call_params={
+                'record': data
+            }
+        )
+        rospy.loginfo(f"Successfully created a call for recording datalog:\n{call}")
+        rospy.loginfo("Creating extrinsic for recording datalog")
+        extrinsic = substrate.create_signed_extrinsic(call=call, keypair=keypair)
+    except Exception as e:
+        rospy.logerr(f"Failed to create an extrinsic for recording datalog: {e}")
+        return None
+
+    try:
+        rospy.loginfo("Submitting extrinsic for recording datalog")
+        receipt = substrate.submit_extrinsic(extrinsic, wait_for_inclusion=True)
+        rospy.loginfo(f"Extrinsic {receipt.extrinsic_hash} for recording datalog sent and included in block"
+                      f" {receipt.extrinsic_hash}")
+        return receipt.extrinsic_hash
+    except Exception as e:
+        rospy.logerr(f"Failed to submit extrinsic for recording datalog: {e}")
+        return None
+
+
+class Baxter:
+    """
+    Robot control class with activation and moving functions
+    """
+
+    def __init__(self):
+        """
+        init all constants and start working
+        """
+        # all constants
+        self.hash = ""
+        self.br = CvBridge()
+        self.dirname = os.path.dirname(__file__)
+        self.path_result = self.dirname + "/result.txt"
+        self.result = []
+        self.i = time.time()
+        self.result_picture = []
+        self.stop_publish = False
+        self.happy_picture = self.dirname + "/happy_smile.jpg"
+        self.done_picture = self.dirname + "/accept.png"
+        self.publish = Thread(target=self.listener)
+        self.result_file = open(self.path_result, "w")
+
+        # parsing config
+        self.config = read_config(self.dirname + "/../../config/config.yaml")
+        self.baxter_address = self.config["baxter_address"]
+        self.baxter_seed = self.config["baxter_seed"]
+        self.employer_address = self.config["employer_address"]
+        self.node_address = self.config["node_address"]
+
+        rospy.loginfo("Activation")
+        rospy.init_node('robot_control', anonymous=False)
+
+        self.face_publisher = rospy.Publisher('/robot/xdisplay', Image, queue_size=1)
+        self.sad_picture = self.dirname + "/sad_face.png"
+        self.face = cv2.imread(self.sad_picture, 1)
+        self.face_msg = self.br.cv2_to_imgmsg(self.face, "bgr8")
+        self.face_publisher.publish(self.face_msg)
+        rospy.loginfo("Activation complete. Ready for a job")
+
+        rospy.loginfo('Initiating substrate connection for launch tracking and datalogs writing')
+        self.substrate_launch = substrate_connection(self.node_address)
+        self.substrate_datalog = substrate_connection(self.node_address)
+        launch_tracker = LaunchTracker(self.substrate_launch, self.employer_address, self.baxter_address)
+
+        rospy.loginfo('Waiting job command from employer, press Ctrl+\\ to interrupt')
+
+        while True:
+            launch_tracker.launch_command_event.wait()
+            self.work()
+            self.hash_result = push_to_ipfs(self.result_picture, self.dirname)
+            for self.hash in self.hash_result:
+                self.tr_hash = write_datalog(self.substrate_datalog, self.baxter_seed, self.hash)
+            launch_tracker.launch_command_event.clear()
+            rospy.loginfo("Published to chain! Transaction hash is " + self.tr_hash)
+            rospy.loginfo("Job Done. Check DAPP for IPFS data hash")
+
+    def work(self) -> None:
+        rospy.loginfo("Start working")
+        self.face = cv2.imread(self.happy_picture, 1)
+        self.face_msg = self.br.cv2_to_imgmsg(self.face, "bgr8")
+        self.face_publisher.publish(self.face_msg)
+        self.publish.start()
+        rospy.sleep(7)
+        self.stop_publish = True
+        self.publish.join()
+
+        for f in self.result:
+            self.result_file.write(f)
+        self.result_file.close()
+
+        self.face = cv2.imread(self.done_picture, 1)
+        self.face_msg = self.br.cv2_to_imgmsg(self.face, "bgr8")
+        self.face_publisher.publish(self.face_msg)
+
+    def callback_head(self, data: Image) -> None:
+        if not self.stop_publish:
+            if time.time() - self.i > 2:
+                self.path_pic = self.dirname + "/scrennshot" + str(int(self.i)) + ".png"
+                self.result_picture.append(self.path_pic)
+                self.image = self.br.imgmsg_to_cv2(data)
+                cv2.imwrite(self.path_pic, self.image)
+                self.i = time.time()
+
+    def callback(self, data: LaserScan) -> None:
+        self.result.append(str(data))
+
+    def listener(self):
+        self.rate = rospy.Rate(2)
+        rospy.Subscriber('/cameras/head_camera/image', Image, self.callback_head)
+        rospy.Subscriber('/sim/laserscan/left_hand_range/state', LaserScan, self.callback)
+        while not rospy.is_shutdown():
+            self.face_publisher.publish(self.face_msg)
+            if self.stop_publish:
+                break
+            self.rate.sleep()
+
+
+class LaunchTracker:
+    """
+    Parse new blocks in chain to search for Launch commands
+    """
+
+    def __init__(self, substrate: tp.Any, source_address: str, target_address: str):
+        """
+        Starting subscriber and creating Python event
+        :param substrate: instance of substrate connection
+        :param source_address: address from which tre launch transaction is supposed
+        :param target_address: robot address
+        """
+        rospy.loginfo("Creating an instance of a LaunchTracker class")
+        self.substrate = substrate
+        self.employer_address = source_address
+        self.robot_address: str = target_address
+
+        rospy.loginfo(f"Initiating new blocks subscriber for launch commands tracking")
+        self.launch_command_event = threading.Event()
+        self.subscriber = threading.Thread(target=self._obtain_launch_commands)
+        self.subscriber.start()
+
+        rospy.loginfo("Block subscriber started. Waiting for launch commands")
+
+    def _subscription_handler(self, obj, update_nr, subscription_id):
+        """
+        parse block events and trigger python Event on launch command sending to the robot address
+        params info:
+        https://github.com/polkascan/py-substrate-interface/blob/65f247f129016f5bb3a9a572579033f35fd385ea/substrateinterface/base.py#L2588
+        """
+        ch: str = self.substrate.get_chain_head()
+        chain_events = self.substrate.get_events(ch)
+        for ce in chain_events:
+            if ce.value["event_id"] == "NewLaunch":
+                print(ce.params)
+            if ce.value["event_id"] == "NewLaunch" and ce.params[0]["value"] == self.employer_address \
+                    and ce.params[1]["value"] == self.robot_address and ce.params[2]["value"] is True:  # yes/no
+                rospy.loginfo(f"\"ON\" launch command from employer.")
+                self.launch_command_event.set()  # trigger python Event in main loop
+
+            elif ce.value["event_id"] == "NewLaunch" and ce.params[0]["value"] != self.employer_address \
+                    and ce.params[1]["value"] == self.robot_address:
+                rospy.loginfo(f"Launch command not from employer. Idle")
+
+    def _obtain_launch_commands(self):
+        """
+        Subscribe to new block headers as soon as they are available. The callable `subscription_handler` will be
+        executed when a new block is available and execution will block until `subscription_handler` will return
+        a result other than `None`
+        """
+
+        self.substrate.subscribe_block_headers(self._subscription_handler)
+
+
+if __name__ == '__main__':
+    baxter = Baxter()
+
+
+'''
 rospy.init_node('robot_control', anonymous=False)
 rospy.loginfo("Activation")
 br = CvBridge()
 dirname = os.path.dirname(__file__)
+
+
 path = dirname + "/configuration.txt"
 conf = open(path, 'r')
 my_private_key = conf.readline()
@@ -29,9 +310,11 @@ face = cv2.imread(sad_picture, 1)
 face_msg = br.cv2_to_imgmsg(face,  "bgr8")
 rospy.loginfo("Activation complete. Ready for a job")
 face_publisher.publish(face_msg)
+
 # end of activation
 
 # waiting for a job
+
 program = '/home/$USER/robot_ws/' + 'robonomics io read launch'
 rob_read = subprocess.Popen(program, shell=True, stdout=subprocess.PIPE)
 while True:
@@ -52,7 +335,9 @@ while True:
     except KeyboardInterrupt:
         exit()
 
+
 # start working
+
 rospy.loginfo("Start working")
 result = ""
 happy_picture = dirname + "/happy_smile.jpg"
@@ -63,6 +348,7 @@ i = time.time()
 result_picture = []
 global stop_publish
 stop_publish = False
+
 
 def callback_head(data):
     global i
@@ -76,9 +362,11 @@ def callback_head(data):
             cv2.imwrite(path, image)
             i = time.time()
 
+
 def callback(data):
     global result
     result = result + (str(data) + "\n")
+
 
 def listener():
     global stop_publish
@@ -90,13 +378,14 @@ def listener():
         face_publisher.publish(face_msg)
         if stop_publish:
             break
-        rate.sleep()
+        rate.sleep()    
 
-publish = Thread(target = listener)
+publish = Thread(target=listener)
 publish.start()
 rospy.sleep(7)
 stop_publish = True
 publish.join()
+
 
 try:
     path = dirname + "/result.txt"
@@ -107,17 +396,18 @@ finally:
     result_file.close()
 rospy.loginfo("End of work")
 
+
 done_picture = dirname + "/accept.png"
 face = cv2.imread(done_picture, 1)
 face_msg = br.cv2_to_imgmsg(face,  "bgr8")
 
-#push to ipfs
+# push to ipfs
 rospy.loginfo("Push to IPFS")
 client = ipfshttpclient.connect()
 hash_result = []
 for x in result_picture:
     while not rospy.is_shutdown():
-        face_publisher.publish(face_msg)
+        # face_publisher.publish(face_msg)
         break
     res = client.add(x)
     hash_result.append(res.values()[0].encode('utf8'))
@@ -125,8 +415,8 @@ for x in result_picture:
 res = client.add(dirname + '/' + "result.txt")
 hash_result.append(res.values()[0].encode('utf8'))
 rospy.loginfo("Pushed, the IPFS hash is " + res.values()[0].encode('utf8'))
-
-#push to robonomics
+    
+# push to robonomics
 rospy.loginfo("Push hash to robonomics")
 for r in hash_result:
     print("echo \"" + r + "\" | " + '/home/$USER/robot_ws/' + "robonomics io write datalog -s " + my_private_key)
@@ -134,3 +424,4 @@ for r in hash_result:
     process = subprocess.Popen(program, shell=True, stdout=subprocess.PIPE)
     rospy.sleep(4)
 rospy.loginfo("Job finished")
+'''
